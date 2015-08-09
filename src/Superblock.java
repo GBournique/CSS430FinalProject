cefbf037de04d197dba554464a9d8537bed16bd5
Kasey Cohen <kcparker@uw.edu>
2015-08-09 14:20:17 -0700

    initial commit

---
+++ src/Superblock.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Michael on 7/23/2015.
+ */
+public class Superblock {
+    public int totalBlocks;
+    public int totalInodes;
+    public int freeList;
+
+    public Superblock(int diskSize) {
+
+    }
+}
